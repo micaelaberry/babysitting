6f4b72d53c273d91f4d602257d0459d4024a308f
Micaela <msberry9@gmail.com>
2017-07-29 13:55:42 -0400

    second test passed

--- src/main/java/BabySitting.java
+++ src/main/java/BabySitting.java
@@ -1,11 +1,18 @@
 
 public class BabySitting {
-
+//	starts no earlier than 5:00PM
+//	leaves no later than 4:00AM
+//	gets paid $12/hour from start-time to bedtime
+//	gets paid $8/hour from bedtime to midnight
+//	gets paid $16/hour from midnight to end of job
+//	gets paid for full hours (no fractional hours)
+	
+	
 	public boolean isValidStartTime(int time) {
-		if (time >= 17 && time <= 4){
+		if (time >= 17 || time <= 4) {
 			return true;
-	}
-	return false;
+		}
+		return false;
 
-}
+	}
 }
--- src/test/java/BabysittingTest.java
+++ src/test/java/BabysittingTest.java
@@ -5,13 +5,18 @@ import org.junit.Test;
 public class BabysittingTest {
 
 	@Test
-	public void whenStartTimeIsAfterFivePm(){
+	public void whenStartTimeIsBeforeFivePm(){
 	BabySitting babysitting = new BabySitting();
 	int startTime = 16;
 	assertEquals(babysitting.isValidStartTime(startTime), false);
 }
 	
-	
+	@Test
+	public void whenStartTimeIsAfterFivePm(){
+	BabySitting babysitting = new BabySitting();
+	int startTime = 18;
+	assertEquals(babysitting.isValidStartTime(startTime), true);
+	}
 	
 }
 
